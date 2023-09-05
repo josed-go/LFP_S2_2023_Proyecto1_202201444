f7ec738a80bfae037779637cf10f2a3fb20de660
José Góngora <jdavidgongorao@gmail.com>
2023-09-04 22:08:21 -0600

    Se agrego numero de lineas

--- app.py
+++ app.py
@@ -2,77 +2,92 @@ import tkinter as tk
 from tkinter import ttk
 import tkinter.font as font
 
-def center_window(window):
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-    x = (screen_width - 1350) // 2
-    y = (screen_height - 850) // 2
-    window.geometry(f"1350x850+{x}+{y}")
+class app:
+    def __init__(self, raiz):
+        self.raiz = raiz
+        self.cantidad_lineas = 1
+        self.raiz.config(bg="#fdf9c4")
+        self.raiz.title("Analizador lexico")
+        self.raiz.resizable(0,0)
+        self.menu_frame = tk.Frame(self.raiz, width="1350", height="60", bg="#FDF9DF")
+        self.menu_frame.pack_propagate(False)
+        self.menu_frame.grid_propagate(False)
+        self.menu_frame.pack()
+
+        self.menu_frame.columnconfigure(0, weight=1)  # Columna vacía
+        self.menu_frame.columnconfigure(1, weight=1)  # Columna para el botón "Analizar"
+        self.menu_frame.columnconfigure(2, weight=1)  # Columna para el botón "Errores"
+        self.menu_frame.columnconfigure(3, weight=1) 
+
+        self.editor_frame = tk.Frame()
+
+        self.editor_frame.pack()
+        self.editor_frame.config(width="1350", height="800", bg="#fdf9c4")
+
+        self.editor = tk.Text(self.editor_frame, width="140", height="39", padx=20, pady=20, font=('Arial', 12))
+        self.editor.grid(row=0, column=0, padx=10, pady=25)
+
+        self.lineas_bar = tk.Text(self.editor_frame, width=3, padx=4, takefocus=0, border=0, background='lightgrey', state='disabled')
+        self.lineas_bar.tag_configure("center", justify="center")
+        self.lineas_bar.tag_add("center", "1.0", "end")
+        self.lineas_bar.grid(row=0, column=0, pady=25, sticky="nsw")
+
+        self.editor.bind('<Key>', self.actualizar_lineas)
+        self.editor.bind('<MouseWheel>', self.actualizar_lineas)
+
+        self.scroll_editor = tk.Scrollbar(self.editor_frame, command=self.editor.yview)
+        self.scroll_editor.grid(row=0, column=0, pady=25, sticky="nse")
+        self.editor.config(yscrollcommand=self.scroll_editor.set)
+
+        self.menu = tk.Menu(self.raiz, background='blue', fg='white')
+
+
+        self.opciones_menu = tk.Menu(self.menu, tearoff=0, background='#fdf9c4')
+        self.opciones_menu.add_command(label="Abrir", background='#fdf9c4')
+        self.opciones_menu.add_command(label="Guardar", background='#fdf9c4')
+        self.opciones_menu.add_command(label="Guardar como", background='#fdf9c4')
+        self.opciones_menu.add_separator(background='#fdf9c4')
+        self.opciones_menu.add_command(label="Salir", background='#fdf9c4', command=self.raiz.quit)
+
+        self.menu.add_cascade(label="Archivo", menu=self.opciones_menu)
+
+        self.fuente = font.Font(weight="bold")
+
+        self.analizar_B = tk.Button(self.menu_frame, text="Analizar", padx=20, height=1, bg="#fdf9c4", activebackground="#ffda9e")
+        self.analizar_B.grid(row=0, column=1, padx=10, pady=10)
+        self.analizar_B['font'] = self.fuente
+
+        self.errores_B = tk.Button(self.menu_frame, text="Errores", padx=20, height=1, bg="#fdf9c4", activebackground="#ffda9e")
+        self.errores_B.grid(row=0, column=2, padx=10, pady=10)
+        self.errores_B['font'] = self.fuente
+
+        self.reporte_B = tk.Button(self.menu_frame, text="Reporte", padx=20, height=1, bg="#fdf9c4", activebackground="#ffda9e")
+        self.reporte_B.grid(row=0, column=3, padx=10, pady=10)
+        self.reporte_B['font'] = self.fuente
+
+        self.opciones_menu.config(background="#fdf9c4")
+        self.raiz.config(menu=self.menu)
+
+    def center_window(self, window):
+        screen_width = window.winfo_screenwidth()
+        screen_height = window.winfo_screenheight()
+        x = (screen_width - 1350) // 2
+        y = (screen_height - 850) // 2
+        window.geometry(f"1350x850+{x}+{y}")
+
+    def actualizar_lineas(self, event = None):
+        cantidad = self.editor.get('1.0', tk.END).count('\n')
+        if cantidad != self.cantidad_lineas:
+            self.lineas_bar.config(state = tk.NORMAL)
+            self.lineas_bar.delete(1.0, tk.END)
+            for linea in range(1, cantidad + 1):
+                self.lineas_bar.insert(tk.END, f"{linea}\n")
+            self.lineas_bar.config(state = tk.DISABLED)
+            self.cantidad_lineas = cantidad
+
 
 raiz = tk.Tk()
-raiz.config(bg="#fdf9c4")
-#raiz.config(bg="#FDF9E0")
-raiz.title("Analizador lexico")
-
-raiz.resizable(0,0)
-#raiz.geometry("1350x850")
-#raiz.eval('tk::PlaceWindow . center')
-
-menu_frame = tk.Frame(raiz, width="1350", height="60", bg="#FDF9DF")
-menu_frame.pack_propagate(False)
-menu_frame.grid_propagate(False)
-menu_frame.pack()
-#menu_frame.config(bg="#ffda9e")
-#menu_frame.config(width="1350", height="50")
-
-menu_frame.columnconfigure(0, weight=1)  # Columna vacía
-menu_frame.columnconfigure(1, weight=1)  # Columna para el botón "Analizar"
-menu_frame.columnconfigure(2, weight=1)  # Columna para el botón "Errores"
-menu_frame.columnconfigure(3, weight=1) 
-
-editor_frame = tk.Frame()
-
-editor_frame.pack()
-editor_frame.config(width="1350", height="800", bg="#fdf9c4")
-
-editor = tk.Text(editor_frame, width="140", height="39", padx=20, pady=20, font=('Arial', 12))
-editor.grid(row=0, column=0, padx=10, pady=25)
-
-scroll_editor = tk.Scrollbar(editor_frame, command=editor.yview)
-scroll_editor.grid(row=0, column=0, pady=25, sticky="nse")
-editor.config(yscrollcommand=scroll_editor.set)
-
-
-menu = tk.Menu(raiz, background='blue', fg='white')
-
-
-opciones_menu = tk.Menu(menu, tearoff=0, background='#fdf9c4')
-opciones_menu.add_command(label="Abrir", background='#fdf9c4')
-opciones_menu.add_command(label="Guardar", background='#fdf9c4')
-opciones_menu.add_command(label="Guardar como", background='#fdf9c4')
-opciones_menu.add_separator(background='#fdf9c4')
-opciones_menu.add_command(label="Salir", background='#fdf9c4', command=raiz.quit)
-
-menu.add_cascade(label="Archivo", menu=opciones_menu)
-
-fuente = font.Font(weight="bold")
-
-analizar_B = tk.Button(menu_frame, text="Analizar", padx=20, height=1, bg="#fdf9c4", activebackground="#ffda9e")
-analizar_B.grid(row=0, column=1, padx=10, pady=10)
-analizar_B['font'] = fuente
-
-errores_B = tk.Button(menu_frame, text="Errores", padx=20, height=1, bg="#fdf9c4", activebackground="#ffda9e")
-errores_B.grid(row=0, column=2, padx=10, pady=10)
-errores_B['font'] = fuente
-
-reporte_B = tk.Button(menu_frame, text="Reporte", padx=20, height=1, bg="#fdf9c4", activebackground="#ffda9e")
-reporte_B.grid(row=0, column=3, padx=10, pady=10)
-reporte_B['font'] = fuente
-
-opciones_menu.config(background="#fdf9c4")
-raiz.config(menu=menu)
-
-
-center_window(raiz)
+ventana = app(raiz)
+ventana.center_window(raiz)
 
 raiz.mainloop()
